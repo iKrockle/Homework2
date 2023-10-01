953d2fe2cc454c281829ae6a783b63d446d2443c
iKrockle <forsakl123@gmail.com>
2023-10-01 14:21:16 +0500

    HW2 task1

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        var dog = 8.0;
+        var cat = 3.6;
+        var paper = 763789;
+        System.out.println("dog="+dog+", cat="+cat+" paper="+paper);
+    }
+}
